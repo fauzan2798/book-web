cbbf63e667cc32ece5dc7f4a71ef1025090c2bb5
risanjayy <risyadul@thelionparcel.com>
2024-09-10 14:05:07 +0700

    slicing subscribe

--- src/components/Subscribe/index.js
+++ src/components/Subscribe/index.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section className='subscribe flex justify-center items-center mt-20'>
+      <div className='p-10'>
+        <h4 className='mb-12'>Recommended books from you to review</h4>
+        <form>
+          <input placeholder='Write your recommendation' className='submit-area'/>
+          <button className='submit-button'>
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
