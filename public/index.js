6c94508c251505785c6e51e214dfc95e9ddea7c2
risanjayy <risyadul@thelionparcel.com>
2024-09-10 10:38:45 +0700

    slicing hero

---
+++ public/index.js
@@ -0,0 +1,46 @@
+import Author1 from "./author1.png"
+import Author2 from "./author2.png"
+import Author3 from "./author3.png"
+import Author4 from "./author4.png"
+import Client1 from "./client1.png"
+import Client2 from "./client2.png"
+import Client3 from "./client3.png"
+import Cover1 from "./cover1.png"
+import Cover2 from "./cover2.png"
+import Cover3 from "./cover3.png"
+import Cover4 from "./cover4.png"
+import Cover5 from "./cover5.png"
+import Cover6 from "./cover6.png"
+import Cover7 from "./cover7.png"
+import Cover8 from "./cover8.png"
+import LogoImg from "./logo.png"
+import HeroImg from "./hero.png"
+import BgImg from "./background.png"
+import IGImg from "./instagram.png"
+import FBImg from "./facebook.png"
+
+export {
+    Author1,
+    Author2,
+    Author3,
+    Author4,
+    Client1,
+    Client2,
+    Client3,
+    Cover1,
+    Cover2,
+    Cover3,
+    Cover4,
+    Cover5,
+    Cover6,
+    Cover7,
+    Cover8,
+    LogoImg,
+    HeroImg,
+    BgImg,
+    IGImg,
+    FBImg,
+}
+
+
+
